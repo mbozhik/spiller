1672a237496959e61b66f7383eb6ce966edabc96
bozzhik <bozzhik@ya.ru>
2024-04-10 00:42:53 +0300

    finalize new `Form` UI component to send email

--- src/components/UI/Form.tsx
+++ src/components/UI/Form.tsx
@@ -1,9 +1,14 @@
-import {useEffect} from 'react'
-
+import React, {useState, useEffect} from 'react'
 import {buttonVariants} from './Button'
 import Title from '#/UI/Title'
 
 const Form = ({onClose}) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [cosmetologist, setCosmetologist] = useState(false)
+  const [message, setMessage] = useState('')
+
   useEffect(() => {
     const handleClickOutside = (event) => {
       const formElement = document.getElementById('FORM_WRAPPER')
@@ -23,14 +28,14 @@ const Form = ({onClose}) => {
 
     const sendData = async () => {
       const data = {
-        name: 'тест',
-        email: 'maxim@mail.md',
-        phone: '+37368787744',
-        cosmetologist: 'NO',
-        message: 'проверка формы',
+        name,
+        email,
+        phone,
+        cosmetologist: cosmetologist ? 'Да' : 'Нет',
+        message,
       }
 
-      const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbxLKd6b5WOEqv_YFxQrIKzpzMrrTsUwNTstFwAY8jZq_KNYmL5jYCaadmJIrSYhNW_-/exec'
+      const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbyUeNos_7YubG8nvQolQQlNbOyuFtv1CoNZeSs-tY-ia5V4LTkUba_E-gDuuZVy_5aX/exec'
 
       try {
         const response = await fetch(GOOGLE_SHEET_URL, {
@@ -56,9 +61,11 @@ const Form = ({onClose}) => {
     sendData()
   }
 
+  const inputStyles = 'text-custom-blue border border-custom-blue w-full p-2 sm:p-3 sm: placeholder:text-custom-blue focus:outline-custom-blue'
+
   return (
     <section className="fixed inset-0 z-50 grid w-screen h-screen place-items-center bg-custom-blue/25">
-      <div id="FORM_WRAPPER" className="w-[25%] xl:w-[35%] sm:w-[95%] p-5 bg-white shadow-nav">
+      <div id="FORM_WRAPPER" className="w-[25%] xl:w-[35%] sm:w-[95%] p-5 sm:p-3 bg-white shadow-nav">
         <div className="flex flex-col gap-2">
           <div className="flex justify-between">
             <Title text="Оставить заявку:" />
@@ -66,8 +73,21 @@ const Form = ({onClose}) => {
               &times;
             </span>
           </div>
-          <form className="mt-5 space-y-1" onSubmit={submitForm}>
-            <button className={`!w-full block text-center -mt-5 ${buttonVariants.default} ${buttonVariants.secondary} `} title="submit">
+          <form className="mt-5 space-y-1 sm:space-y-3" onSubmit={submitForm}>
+            <div className="space-y-4">
+              <input className={inputStyles} placeholder="Имя" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+              <input className={inputStyles} placeholder="E-mail" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <input className={inputStyles} placeholder="Телефон" type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
+
+              <label className="flex gap-3 sm:gap-4 select-none sm:text-sm">
+                Являетесь ли вы косметологом?
+                <input className="w-4" type="checkbox" checked={cosmetologist} onChange={(e) => setCosmetologist(e.target.checked)} />
+              </label>
+
+              <textarea className={inputStyles} placeholder="Текст" value={message} onChange={(e) => setMessage(e.target.value)} rows={5}></textarea>
+            </div>
+
+            <button className={`!w-full block text-center ${buttonVariants.default} ${buttonVariants.secondary} `} title="submit">
               Отправить
             </button>
           </form>
