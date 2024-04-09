0d0059503c725fd95032fbf9468edb50f53c522a
bozzhik <bozzhik@ya.ru>
2024-04-10 00:57:46 +0300

    minor optimize `Form` UI component

--- src/components/UI/Form.tsx
+++ src/components/UI/Form.tsx
@@ -1,8 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
+
 import {buttonVariants} from './Button'
 import Title from '#/UI/Title'
 
 const Form = ({onClose}) => {
+  const [submitMessage, setSubmitMessage] = useState('')
+
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [phone, setPhone] = useState('')
@@ -51,10 +54,15 @@ const Form = ({onClose}) => {
         }
 
         const responseData = await response.json()
-        console.log(data)
         console.log('Response Data:', responseData)
+
+        setSubmitMessage('Форма отправлена!')
+        setTimeout(() => {
+          onClose()
+        }, 2000)
       } catch (error) {
         console.error('Error:', error)
+        setSubmitMessage('Ошибка')
       }
     }
 
@@ -66,32 +74,36 @@ const Form = ({onClose}) => {
   return (
     <section className="fixed inset-0 z-50 grid w-screen h-screen place-items-center bg-custom-blue/25">
       <div id="FORM_WRAPPER" className="w-[25%] xl:w-[35%] sm:w-[95%] p-5 sm:p-3 bg-white shadow-nav">
-        <div className="flex flex-col gap-2">
-          <div className="flex justify-between">
-            <Title text="Оставить заявку:" />
-            <span className="-mt-1 text-4xl leading-none duration-200 cursor-pointer hover:text-custom-blue" onClick={onClose}>
-              &times;
-            </span>
-          </div>
-          <form className="mt-5 space-y-1 sm:space-y-3" onSubmit={submitForm}>
-            <div className="space-y-4">
-              <input className={inputStyles} placeholder="Имя" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-              <input className={inputStyles} placeholder="E-mail" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-              <input className={inputStyles} placeholder="Телефон" type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
-
-              <label className="flex gap-3 sm:gap-4 select-none sm:text-sm">
-                Являетесь ли вы косметологом?
-                <input className="w-4" type="checkbox" checked={cosmetologist} onChange={(e) => setCosmetologist(e.target.checked)} />
-              </label>
-
-              <textarea className={inputStyles} placeholder="Текст" value={message} onChange={(e) => setMessage(e.target.value)} rows={5}></textarea>
+        {submitMessage ? (
+          <Title text={submitMessage} classes="text-center" />
+        ) : (
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between">
+              <Title text="Оставить заявку:" />
+              <span className="-mt-1 text-4xl leading-none duration-200 cursor-pointer hover:text-custom-blue" onClick={onClose}>
+                &times;
+              </span>
             </div>
+            <form className="mt-5 space-y-1 sm:space-y-3" onSubmit={submitForm}>
+              <div className="space-y-4">
+                <input className={inputStyles} placeholder="Имя" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                <input className={inputStyles} placeholder="E-mail" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input className={inputStyles} placeholder="Телефон" type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
 
-            <button className={`!w-full block text-center ${buttonVariants.default} ${buttonVariants.secondary} `} title="submit">
-              Отправить
-            </button>
-          </form>
-        </div>
+                <label className="flex gap-3 sm:gap-4 select-none sm:text-sm">
+                  Являетесь ли вы косметологом?
+                  <input className="w-4" type="checkbox" checked={cosmetologist} onChange={(e) => setCosmetologist(e.target.checked)} />
+                </label>
+
+                <textarea className={inputStyles} placeholder="Текст" value={message} onChange={(e) => setMessage(e.target.value)} rows={5}></textarea>
+              </div>
+
+              <button className={`!w-full block text-center ${buttonVariants.default} ${buttonVariants.secondary} `} title="submit">
+                Отправить
+              </button>
+            </form>
+          </div>
+        )}
       </div>
     </section>
   )
