e7c79c32ee7947706dbf1e8097bae6c66269f179
bozzhik <bozzhik@ya.ru>
2024-04-12 02:42:22 +0300

    add `.ndjson` output for `get_data.py`

--- python/get_data.py
+++ python/get_data.py
@@ -13,25 +13,36 @@ prev_usage = None
 
 # Iterate through objectData array and format the data
 # If [name] is empty, replace it with the previous non-empty [name]
-
 for obj in data["objectData"]:
+    # Remove extra spaces from the "name" field
+    obj["name"] = ' '.join(obj["name"].split())
+
     if not obj["name"]:
         obj["name"] = prev_name
     else:
         prev_name = obj["name"]
-    
+
     if not obj["description"]:
         obj["description"] = prev_description
     else:
         prev_description = obj["description"]
-    
+
     if not obj["usage"]:
         obj["usage"] = prev_usage
     else:
         prev_usage = obj["usage"]
 
-output_file = "python/formatted_data.json"
-with open(output_file, "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+formatted_data = data["objectData"]
 
-print(f"Formatted data has been written to {output_file}")
+output_json_file = "python/formatted_data.json"
+with open(output_json_file, "w", encoding="utf-8") as json_file:
+    json.dump(formatted_data, json_file, ensure_ascii=False, indent=4)
+
+output_ndjson_file = "python/formatted_data.ndjson"
+with open(output_ndjson_file, "w", encoding="utf-8") as ndjson_file:
+    for item in formatted_data:
+        ndjson_file.write(json.dumps(item, ensure_ascii=False) + '\n')
+
+print("\033[31m" + "###")
+print("\033[35m" + f"Formatted data has been written to {output_json_file}")
+print("\033[31m" + "###")
