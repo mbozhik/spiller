77fdf441a59289dfe8c60534b425e64ca7db3d25
bozzhik <bozzhik@ya.ru>
2024-04-12 02:53:09 +0300

    add new `keys` for `get_data.py` output

--- python/get_data.py
+++ python/get_data.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+import random
 
 url = "https://script.googleusercontent.com/macros/echo?user_content_key=c6WklQpKaqkX6CpcKECPePcH2jGE-R8po4iwuZB_jJyt9Mei3lKCd-YDFqMr6tOgNDzQFR9GKqMDYRoeqXExIQE9SGOvFpvAm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnKVGZTHI_Nwxc5R_GMMIuVvd4M-QlgV3c755jirSdPP7XhD9pzQbvtbRvtpJS283DA1kEFBQe8SXJpuPbLSZyAQpTEda1UDEcA&lib=MsvCZFAvZ-7jT9ejadsDEN0cg7Il6qISr"
 response = requests.get(url)
@@ -11,8 +12,12 @@ prev_name = None
 prev_description = None
 prev_usage = None
 
-# Iterate through objectData array and format the data
-# If [name] is empty, replace it with the previous non-empty [name]
+# New keys and their possible values
+gender_values = ["мужской", "женский"]
+body_part_values = ["рука", "тело"]
+age_group_values = ["молодая", "зрелая", "возрастная"]
+product_compexity_values = ["базовые решения", "комплексные", "сложные препараты"]
+
 for obj in data["objectData"]:
     # Remove extra spaces from the "name" field
     obj["name"] = ' '.join(obj["name"].split())
@@ -31,6 +36,12 @@ for obj in data["objectData"]:
         obj["usage"] = prev_usage
     else:
         prev_usage = obj["usage"]
+
+    obj["gender"] = random.choice(gender_values)
+    obj["body_part"] = random.choice(body_part_values)
+    obj["age_group"] = random.choice(age_group_values)
+    obj["product_compexity"] = random.choice(product_compexity_values)
+    obj["is_ampulsive"] = bool(random.getrandbits(1))  # Random boolean value
 
 formatted_data = data["objectData"]
 
