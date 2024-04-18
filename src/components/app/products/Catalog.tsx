f6b022057f2495f255073b289ad413ade67b1b03
bozzhik <bozzhik@ya.ru>
2024-04-18 18:30:37 +0300

    remake `Catalog` component due changed `product` entity

--- src/components/app/products/Catalog.tsx
+++ src/components/app/products/Catalog.tsx
@@ -4,44 +4,54 @@ import Link from 'next/link'
 import Image from 'next/image'
 import {client, urlForImage} from '@/lib/sanity'
 
+import {compareCategory, categoryTitles} from '@/lib/categories'
+
 interface Product {
   name: string
+  caption: string
   article: number
   volume: string
   description: string
   usage: string
+  image: Array<{asset: {url: string}}>
   price: number
   gender: string
   body_part: string
   age_group: string
-  complexity: string
-  ampulsivity: boolean
+  skin_type: string
+  problem_action: string
+  product_type: string
+  slug: string
+  [x: string]: any
 }
 
 const getData = async (): Promise<Product[]> => {
   noStore()
 
   const query = `
-    *[_type == 'product'] {
+    *[_type == 'product' && slug.current != null] {
         name,
+        caption,
         article,
         volume,
         description,
         usage,
-        images,
+        image,
         price,
         gender,
         body_part,
         age_group,
-        complexity,
-        ampulsivity,
+        skin_type,
+        problem_action,
+        product_type,
+        slug,
     }`
 
   const data: Product[] = await client.fetch(query)
   return data
 }
 
-const Program = async () => {
+const Catalog = async () => {
   const products: Product[] = await getData()
 
   if (!products) {
@@ -50,28 +60,25 @@ const Program = async () => {
 
   return (
     <section className="grid w-[70%] mx-auto sm:w-full grid-cols-3 sm:grid-cols-1 items-start gap-5 p-5 m-5 mt-20">
-      {products.map((program, idx) => (
-        <div key={idx} className="flex flex-col justify-between w-full gap-5 p-5 border-2 border-custom-teal group">
-          {/* {program.images && program.images.length > 0 && (
-            <div className="w-full h-[250px] relative self-center">
-              <Image src={urlForImage(program.images[0]).url()} className="object-cover" fill={true} alt={`program 0`} />
-            </div>
-          )} */}
+      {products.map((item, idx) => (
+        <div key={idx} className="flex flex-col justify-between w-full h-full gap-5 p-5 border-2 border-neutral-200  group">
+          <div className="h-[200px] w-full relative self-center">
+            <Image src={urlForImage(item.image).url()} className="object-contain" fill={true} alt={`item 0`} />
+          </div>
 
           <div className="space-y-2">
-            <h1 className="text-2xl font-medium">{program.name}</h1>
-            <mark>{program.article}</mark>
-            <h2 className="underline">{program.volume}</h2>
-            <h2 className="line-clamp-3">{program.description}</h2>
-            <div className="p-2 border-2 border-custom-teal">
-              <h2 className="line-clamp-3">{program.usage}</h2>
+            <div className="border-2 px-1 border-neutral-200 w-fit">{item.article}</div>
+            <h1 className="text-2xl font-semibold">{item.name}</h1>
+            <h1>{item.caption}</h1>
+            <h2 className="underline">{item.volume}</h2>
+            <h2 className="underline">{item.price} тенге</h2>
+
+            <div className="flex flex-wrap gap-1">
+              {Object.keys(categoryTitles).map((category, idx) =>
+                // utility function to compare category value with title
+                compareCategory(category, item[category], idx),
+              )}
             </div>
-            <h2 className="underline">{program.price}</h2>
-            <h2>{program.gender}</h2>
-            <h2>{program.body_part}</h2>
-            <h2>{program.age_group}</h2>
-            <h2>{program.complexity}</h2>
-            <h2>{program.ampulsivity ? 'ампульсивный' : 'не ампульсивный'}</h2>
           </div>
         </div>
       ))}
@@ -79,4 +86,4 @@ const Program = async () => {
   )
 }
 
-export default Program
+export default Catalog
---
+++ src/lib/categories.tsx
@@ -0,0 +1,60 @@
+export const compareCategory = (category: string, value: string, idx: number) => {
+  const title = categoryTitles[category][value]
+  return <mark key={idx}>{title}</mark>
+}
+
+export const categoryTitles = {
+  gender: {
+    female: 'Женский',
+    male: 'Мужской',
+  },
+  body_part: {
+    face: 'Лицо',
+    body: 'Тело',
+  },
+  age_group: {
+    young: 'Молодая',
+    mature: 'Зрелая',
+    aging: 'Возрастная',
+  },
+  skin_type: {
+    normal: 'Нормальная',
+    dry: 'Сухая',
+    problematic: 'Проблемная',
+    combination: 'Комбинированная',
+    'all-types': 'Все типы',
+  },
+  problem_action: {
+    acne: 'Акне',
+    'age-changes': 'Возрастные изменения',
+    pigmentation: 'Пигментация',
+    dryness: 'Сухость',
+    'sun-exposure': 'Воздействие солнца',
+    cleansing: 'Очищение',
+    toning: 'Тонизация',
+    exfoliation: 'Эксфолиация',
+  },
+  product_type: {
+    'ampoule-concentrate': 'Ампульный концентрат',
+    balm: 'Бальзам',
+    gel: 'Гель',
+    deodorant: 'Дезодорант',
+    cream: 'Крем',
+    lotion: 'Лосьон',
+    'light-cream': 'Лёгкий крем',
+    mask: 'Маска',
+    oil: 'Масло',
+    soap: 'Мыло',
+    patches: 'Патчи',
+    foam: 'Пенка',
+    'dense-cream': 'Плотный крем',
+    'gift-set': 'Подарочный набор',
+    'powder-masks': 'Порошковые маски',
+    scrub: 'Скраб',
+    spray: 'Спрей',
+    serum: 'Сыворотка',
+    toner: 'Тоник',
+    shampoo: 'Шампунь',
+    emulsion: 'Эмульсия',
+  },
+}
