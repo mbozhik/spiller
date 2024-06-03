40496f017a761ea950554f8de3a5bc07d16042bd
bozzhik <bozzhik@ya.ru>
2024-06-03 20:43:01 +0300

    edit `CatalogCard` child component

--- src/components/app/products/CatalogCard.tsx
+++ src/components/app/products/CatalogCard.tsx
@@ -1,11 +1,12 @@
 import {urlForImage} from '@/lib/sanity'
+
 import Image from 'next/image'
+import Button from '#/UI/Button'
 
 interface CatalogCardProps {
   item: {
     name: string
     caption: string
-    article: number
     slug: {current: string}
     image?: Array<{asset: {url: string}}>
   }
@@ -17,18 +18,18 @@ const CatalogCard: React.FC<CatalogCardProps> = ({item, idx}) => {
 
   return (
     <article className="flex flex-col justify-between w-full h-full gap-5 p-5 border border-neutral-200 group" key={idx}>
-      <div className="h-[200px] w-full relative self-center">
+      <div className="h-[250px] w-full relative self-center">
         <Image src={urlForImage(item.image).url()} className="object-contain" fill={true} alt={`item ${idx}`} />
       </div>
-      <div className="space-y-2">
-        <div>
-          <h1 className="text-2xl font-semibold">{item.name}</h1>
-          <h1>{item.caption}</h1>
-        </div>
-        <div className="flex gap-2">
-          <div className="border-2 px-1 border-neutral-200 w-fit">{item.article}</div>
-          <div className="border-2 px-1 border-neutral-200 w-fit">{item.slug.current}</div>
-        </div>
+
+      <div className="space-y-1">
+        <h1 className="text-2xl font-semibold">{item.name}</h1>
+        <h1 className="text-custom-grey2">{item.caption}</h1>
+      </div>
+
+      <div className="grid w-full grid-cols-2 gap-2">
+        <Button text="Подробнее" classes="!text-base w-full" href={`/products/${item.slug.current}`} />
+        <Button text="В корзину" classes="!text-base w-full" variant="secondary" />
       </div>
     </article>
   )
