8debb3249b22a1be7bef5f6a2ff9132ebd87d2c1
maxim <bozzhik@ya.ru>
2024-06-15 02:20:07 +0300

    add `Form` partnership block

--- src/app/partnership/page.tsx
+++ src/app/partnership/page.tsx
@@ -5,11 +5,13 @@ export const metadata: Metadata = {
 
 import Container from '#/Global/Container'
 import Intro from '##/partnership/Intro'
+import Form from '##/partnership/Form'
 
 export default function About() {
   return (
     <Container marginBottom={true} className="space-y-14 sm:space-y-5">
       <Intro />
+      <Form />
     </Container>
   )
 }
