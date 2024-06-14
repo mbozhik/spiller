bc95c41ee6a010f6ce227c46242b878550641e98
maxim <bozzhik@ya.ru>
2024-06-15 01:10:44 +0300

    refactor `highlight` functionality of `NavBar` Global child component

--- src/components/Global/Header/NavBar.tsx
+++ src/components/Global/Header/NavBar.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link'
 import {usePathname} from 'next/navigation'
 
 export default function NavBar() {
-  const currentRoute = usePathname()
+  const pathname = usePathname()
 
   const navItems = {
     index: {href: '/', text: 'Главная'},
-    products: {href: '/products', text: 'Продукты'},
-    about: {href: '/about', text: 'О бренде'},
-    buy: {href: '/buy', text: 'Где купить'},
+    products: {href: 'products', text: 'Продукты'},
+    about: {href: 'about', text: 'О бренде'},
+    buy: {href: 'buy', text: 'Где купить'},
   }
 
   const linkStyle = 'py-2 duration-200 border-l-2 xl:py-2 px-8 sm:px-0 hover:bg-custom-nav border-custom-nav w-fit sm:w-[25%]'
@@ -20,12 +20,13 @@ export default function NavBar() {
     <nav className="z-10 flex justify-center text-lg text-center bg-white sm:justify-between sm:text-xs xl:text-base text-custom-grey shadow-nav sm:shadow-nav_mobile">
       {Object.keys(navItems).map((id) => {
         const {href, text} = navItems[id]
-        const isActive = currentRoute === href
+        const fullHref = href === '/' ? href : `/${href}`
+        const isActive = pathname === fullHref
 
         const linkClasses = [linkStyle, isActive && activeStyle, id === 'buy' && 'border-r-2'].filter(Boolean).join(' ')
 
         return (
-          <Link href={href} key={id} className={linkClasses}>
+          <Link href={fullHref} key={id} className={linkClasses}>
             {text}
           </Link>
         )
