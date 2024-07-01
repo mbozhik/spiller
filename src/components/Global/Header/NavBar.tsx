60f122f641d89d2f96a26be36c7a5ec326ba4a4b
maxim <bozzhik@ya.ru>
2024-07-01 17:40:46 +0300

    add link to `partnership` in `NavBar` Header sub-component

--- src/components/Global/Header/NavBar.tsx
+++ src/components/Global/Header/NavBar.tsx
@@ -11,6 +11,7 @@ export default function NavBar() {
     products: {href: 'products', text: 'Продукты'},
     about: {href: 'about', text: 'О бренде'},
     buy: {href: 'buy', text: 'Где купить'},
+    partnership: {href: 'partnership', text: 'Партнёрам'},
   }
 
   const linkStyle = 'py-2 duration-200 border-l-2 xl:py-2 px-8 sm:px-0 hover:bg-custom-nav border-custom-nav w-fit sm:w-[25%]'
@@ -18,12 +19,13 @@ export default function NavBar() {
 
   return (
     <nav className="z-10 flex justify-center text-lg text-center bg-white sm:justify-between sm:text-xs xl:text-base text-custom-grey shadow-nav sm:shadow-nav_mobile">
-      {Object.keys(navItems).map((id) => {
+      {Object.keys(navItems).map((id, index, array) => {
         const {href, text} = navItems[id]
         const fullHref = href === '/' ? href : `/${href}`
         const isActive = pathname === fullHref
+        const isLast = index === array.length - 1
 
-        const linkClasses = [linkStyle, isActive && activeStyle, id === 'buy' && 'border-r-2'].filter(Boolean).join(' ')
+        const linkClasses = [linkStyle, isActive && activeStyle, isLast && 'border-r-2', id === 'about' && 'sm:hidden'].filter(Boolean).join(' ')
 
         return (
           <Link href={fullHref} key={id} className={linkClasses}>
--- src/components/app/index/Partnership.tsx
+++ src/components/app/index/Partnership.tsx
@@ -6,7 +6,7 @@ export default function Partnership() {
   return (
     <section data-section="INTRO" className="flex flex-col gap-5">
       <Title text="ПРИГЛАШАЕМ К СОТРУДНИЧЕСТВУ" />
-      <Text text='Мы, компания "WEC Group", являемся официальным и эксклюзивным дистрибьютором продукции бренда Dr.Spiller. В связи с этим мы активно ищем новых партнёров для сотрудничества.' />
+      <Text text='"WEC Group" является официальным и эксклюзивным дистрибьютором продукции бренда Dr.Spiller. В связи с этим мы активно ищем новых партнёров в Казахстане и Кыргызстане.' />
 
       <Button href="/partnership/" classes="w-full" text="УЗНАТЬ ПОДРОБНЕЕ" />
     </section>
