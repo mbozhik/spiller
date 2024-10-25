8b9ae4318d1a8cfbd0f34d7ca5a5fc6c016fd195
maxim <bozzhik@ya.ru>
2024-10-26 01:28:50 +0300

    add `categories` output for RSS route

--- src/lib/feed.ts
+++ src/lib/feed.ts
@@ -1,22 +1,39 @@
 import {urlForImage} from '@/lib/sanity'
 import {TProduct} from '@/lib/get_products'
+import {product as productFilters} from '@/lib/categories_[product.ts]'
+
+function escapeXML(value: string) {
+  return value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;')
+}
 
 export function generateXML(products: TProduct[]) {
+  const mainFilters = productFilters.find((filter: any) => filter.name === 'main_filter')?.options?.list || []
+
+  const categories = `
+    <categories>
+      ${mainFilters
+        .map(
+          (filter: {title: string; value: string}, index: number) => `
+        <category id="${filter.value}">${escapeXML(filter.title)}</category>`,
+        )
+        .join('')}
+    </categories>`
+
   const offers = products
     .map(
-      (product, index) => `
+      (product) => `
       <offer id="${product.slug.current}" available="true">
-      <url>https://dr-spiller.kz/products/${product.slug.current}</url>
-      <price>${product.discount_price || product.price}</price>
-      <oldprice>${product.price}</oldprice>
-      <currencyId>KZT</currencyId>
-      <categoryId>${product.main_filter}</categoryId>
-      <picture>${urlForImage(product.image).url()}</picture>
-      <name>${product.name}</name>
-      <description>${product.full_description}</description>
-      <sales_notes>Скидки до 50%. Бесплатная доставка в день заказа!</sales_notes>
-      <country_of_origin>Германия</country_of_origin>
-    </offer>`,
+        <url>https://dr-spiller.kz/products/${product.slug.current}</url>
+        <price>${product.discount_price || product.price}</price>
+        <oldprice>${product.price}</oldprice>
+        <currencyId>KZT</currencyId>
+        <categoryId>${product.main_filter}</categoryId>
+        <picture>${urlForImage(product.image).url()}</picture>
+        <name>${escapeXML(product.name)}</name>
+        <description>${escapeXML(product.full_description || '')}</description>
+        <sales_notes>Скидки до 50%. Бесплатная доставка в день заказа!</sales_notes>
+        <country_of_origin>Германия</country_of_origin>
+      </offer>`,
     )
     .join('')
 
@@ -27,6 +44,7 @@ export function generateXML(products: TProduct[]) {
     <currencies>
       <currency id="KZT" rate="1"/>
     </currencies>
+    ${categories}
     <offers>
       ${offers}
     </offers>
