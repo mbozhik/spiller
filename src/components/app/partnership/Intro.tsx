05e3fbac276a0829454ece3e23797acc97ab43f2
maxim <bozzhik@ya.ru>
2024-11-15 22:32:32 +0300

    refactor `Intro` partnership component

--- src/components/app/partnership/Intro.tsx
+++ src/components/app/partnership/Intro.tsx
@@ -1,31 +1,29 @@
-import Image, {StaticImageData} from 'next/image'
 import PartnershipImage from '%/partnership/1.svg'
 import PartnershipImage2 from '%/partnership/2.svg'
 import PartnershipImage3 from '%/partnership/3.svg'
 import PartnershipImage4 from '%/partnership/4.svg'
 
+import Image from 'next/image'
 import Title from '#/UI/Title'
 
-const partnershipData: {[key: number]: {text: string; image: string | StaticImageData}} = {
-  1: {
+const partnershipData = [
+  {
     text: 'Косметологи',
     image: PartnershipImage2,
   },
-  2: {
+  {
     text: 'Салоны красоты <br> и СПА-центры',
     image: PartnershipImage,
   },
-  3: {
+  {
     text: 'Магазины косметики <br> премиум класса',
     image: PartnershipImage3,
   },
-  4: {
+  {
     text: 'Косметологические <br> клиники',
     image: PartnershipImage4,
   },
-}
-
-const getHtml = (text: string) => ({__html: text})
+]
 
 export default function Intro() {
   return (
@@ -33,11 +31,10 @@ export default function Intro() {
       <Title classes="text-center sm:text-left" text="ПРИГЛАШАЕМ К СОТРУДНИЧЕСТВУ" />
 
       <div className="flex justify-between gap-5 w-full sm:flex-col">
-        {Object.values(partnershipData).map(({text, image}) => (
-          <div key={text} className="flex gap-4 items-center">
+        {partnershipData.map(({text, image}, index) => (
+          <div key={index} className="flex gap-4 items-center">
             <Image className="s-12" src={image} alt="" />
-
-            <h1 className="text-lg leading-tight " dangerouslySetInnerHTML={getHtml(text)} />
+            <h1 className="text-lg leading-tight" dangerouslySetInnerHTML={{__html: text}} />
           </div>
         ))}
       </div>
