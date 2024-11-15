2e443c08894fed38adfeeab27b86ca6204ccccbb
maxim <bozzhik@ya.ru>
2024-11-15 15:18:48 +0300

    refactor `NavBar` Global sub-component

--- src/components/Global/Header/NavBar.tsx
+++ src/components/Global/Header/NavBar.tsx
@@ -1,36 +1,34 @@
 'use client'
 
-import Link from 'next/link'
+import {cn} from '@/lib/utils'
+import {isMobile} from '@bozzhik/is-mobile'
 import {usePathname} from 'next/navigation'
+
+import Link from 'next/link'
 
 export default function NavBar() {
   const pathname = usePathname()
 
-  const navItems = {
-    index: {href: '/', text: 'Главная'},
-    products: {href: 'products', text: 'Продукты'},
-    cosmetology: {href: 'cosmetology', text: 'Косметология'},
-    about: {href: 'about', text: 'О бренде'},
-    buy: {href: 'buy', text: 'Где купить'},
-    partnership: {href: 'partnership', text: 'Партнёрам'},
-  }
-
-  const linkStyle = 'py-2 duration-200 border-l-2 xl:py-2 px-8 sm:px-0 hover:bg-custom-nav border-custom-nav w-fit sm:w-[25%]'
-  const activeStyle = 'bg-custom-nav'
+  const navItems = [
+    {href: '/', label: 'Главная'},
+    {href: '/products', label: 'Продукты'},
+    {href: '/cosmetology', label: 'Косметология'},
+    {href: '/about', label: 'О бренде'},
+    {href: '/buy', label: 'Где купить'},
+    {href: '/partnership', label: 'Партнёрам'},
+  ]
 
   return (
     <nav className="z-10 flex justify-center text-lg text-center bg-white sm:justify-between sm:text-xs xl:text-base text-custom-grey shadow-nav sm:shadow-nav_mobile">
-      {Object.keys(navItems).map((id, index, array) => {
-        const {href, text} = navItems[id]
-        const fullHref = href === '/' ? href : `/${href}`
-        const isActive = pathname === fullHref
-        const isLast = index === array.length - 1
+      {navItems.map(({href, label}, index) => {
+        const isActive = pathname === href
+        const isLast = index === navItems.length - 1
 
-        const linkClasses = [linkStyle, isActive && activeStyle, isLast && 'border-r-2', id === 'about' && 'sm:hidden'].filter(Boolean).join(' ')
+        if (isMobile && ['/about', '/'].includes(href)) return null
 
         return (
-          <Link href={fullHref} key={id} className={linkClasses}>
-            {text}
+          <Link href={href} key={href} className={cn('py-2 duration-200 border-l-2 xl:py-2 px-8 sm:px-1.5 hover:bg-custom-nav border-custom-nav w-fit sm:w-full', isActive && 'bg-custom-nav', isLast && 'border-r-2')}>
+            {label}
           </Link>
         )
       })}
