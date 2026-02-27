b4a75ab47fd6fd87581dbc81d878749b86b91332
maxim <bozzhik@gmail.com>
2026-02-27 17:14:30 +0200

    update `address` into `Map` buy component

--- src/components/app/buy/Map.tsx
+++ src/components/app/buy/Map.tsx
@@ -6,9 +6,10 @@ export default function Map() {
     <section data-section="MAP" className="flex flex-col gap-5">
       <Title text="РАСПОЛОЖЕНИЕ ОФИСА:" classes="mb-5" />
       <div className="relative w-full h-[50vh] sm:h-[40vh] overflow-hidden">
-      <iframe className='relative s-full' src="https://yandex.ru/map-widget/v1/?ll=76.950442%2C43.231802&mode=search&oid=126663847614&ol=biz&utm_source=share&z=17.5" width="560" height="400" allowFullScreen={true}></iframe>
+        <iframe className="relative s-full" src="https://yandex.ru/map-widget/v1/?ll=76.949328%2C43.231488&mode=search&oid=117930940758&ol=biz&utm_source=share&z=17.5" width="560" height="400" allowFullScreen={true} />
       </div>
-      <Button text="ПЕРЕЙТИ НА КАРТЫ" blank={true} href="https://yandex.ru/maps/-/CDeKmOne" classes="w-full" />
+
+      <Button text="ПЕРЕЙТИ НА КАРТЫ" blank={true} href="https://yandex.ru/maps/-/CPeteNIV" classes="w-full" />
     </section>
   )
 }
